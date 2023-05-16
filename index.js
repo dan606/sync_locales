b65e94f4a095be8844339d98a9d64904773798b6
Daniel Hron <daniel.hron@ys.cz>
2023-05-16 14:44:42 +0200

    uprava, spusti se npm run start

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-import sync from 'i18next-json-sync'
+const sync = require('i18next-json-sync').default;
+//or in ES5 world:
+//const sync = require('i18next-json-sync').default;
+
+//defaults are inline:
 sync({
-  files: 'locales/*.json',
-  primary: 'cz'
-});
+  /** Audit files in memory instead of changing them on the filesystem and
+    * throw an error if any changes would be made */
+  check: false,
+  /** Glob pattern for the resource JSON files */
+  files: '**/locales/*.json',
+  /** An array of glob patterns to exclude from the files search */
+  excludeFiles: ['**/node_modules/**'],
+  /** Primary localization language. Other language files will be changed to match */
+  primary: 'cz',
+  /** Use empty string for new keys instead of the primary language value */
+  newKeysEmpty: true
+})
